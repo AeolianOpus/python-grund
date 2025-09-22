c59e1918f939529485e23239128fe17b47b9e7e6
Constantine <143692681+AeolianOpus@users.noreply.github.com>
2025-09-22 13:12:41 +0200

    Test

---
+++ lektion-2/data_types/dt_exercises_test.py
@@ -0,0 +1,40 @@
+int_var = 10
+
+float_var = 10.5
+
+complex_var = 3 + 4j
+
+print (int_var, type(int_var))
+print (float_var, type(float_var))
+print (complex_var, type(complex_var))
+
+my_string="Python is fun"
+
+print (my_string[0])
+print (my_string[1])
+print (my_string[-1])
+
+my_list=[1, 2, 3, "Python", "AI"]
+
+print (my_list[0])
+print (my_list[-1])
+print (my_list[-2])
+
+my_tuple = ("apple", "banana", "cherry")
+
+print (my_tuple[0])
+print (my_tuple[-1])
+
+is_true=True
+is_false=False
+
+print (is_true)
+print (is_false)
+
+my_set = {"apple", "banana", "cherry", "apple"}
+my_set.add("orange")
+print (my_set)
+
+my_dict={1: "One", 2: "Two", 3: "Three"}
+print (my_dict[2])
+print (my_dict.get(3))
---
+++ lektion-2/variables/v_exercises_test.py
@@ -0,0 +1,22 @@
+city = "Stockholm"
+print (city)
+
+Age = 25
+age = 30
+print(Age)
+print(age)
+
+x = 42
+print (x)
+x = "Now Im a string"
+print (x)
+
+a, b, c = 10, 20, "Thirty"
+print (a, b, c)
+
+num_str = "123"
+num_int = int(num_str)
+num_float = float(num_str)
+print (num_str, type(int))
+print (num_float, type(float))
+
